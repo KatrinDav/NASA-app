ed338b55f3526549c8e6b8f97c1f5302d2d06770
Kasia Dav <k.dawidowicz2016@gmail.com>
2020-06-21 19:58:13 +0200

    Updates

---
+++ precache-manifest.8cd68cd11b2e209efc916547acbd49fa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8815e3970cb7ab9b02b925e8fb7f472",
+    "url": "/NASA-app/index.html"
+  },
+  {
+    "revision": "b6a30b04e0c416d10222",
+    "url": "/NASA-app/static/css/main.48710b52.chunk.css"
+  },
+  {
+    "revision": "16f2cff3e06e3a1f94e2",
+    "url": "/NASA-app/static/js/2.239a4084.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/NASA-app/static/js/2.239a4084.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6a30b04e0c416d10222",
+    "url": "/NASA-app/static/js/main.990607fd.chunk.js"
+  },
+  {
+    "revision": "1c2a75372bee07965afc",
+    "url": "/NASA-app/static/js/runtime-main.4dc81588.js"
+  },
+  {
+    "revision": "022f27b0274aafab94aa54764e131f6f",
+    "url": "/NASA-app/static/media/img8.022f27b0.jpg"
+  },
+  {
+    "revision": "6099842fdaa23fbcaa3260f73c528545",
+    "url": "/NASA-app/static/media/nasa-logo.6099842f.png"
+  }
+]);
