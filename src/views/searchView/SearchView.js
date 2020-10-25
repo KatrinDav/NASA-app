8857a2b473f1435642cc29ccb95d9dadb079acd1
Kasia Dav <k.dawidowicz2016@gmail.com>
2020-10-25 20:50:40 +0100

    remove debaunce void, add handler

--- src/views/searchView/SearchView.js
+++ src/views/searchView/SearchView.js
@@ -1,35 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import ImagesList from '../../components/ImagesList/ImagesList';
 import Form from '../../components/Form/Form';
 import styles from './SearchView.module.scss';
 
-import useDebounce from '../../utils/use-debounce';
 
 const SearchView = () => {
+
     const [query, setQuery] = useState('');
     const [data, setData] = useState([]);
-    const [isSearching, setIsSearching] = useState(false);
 
-    const debouncedSearchTerm = useDebounce(query, 2000);
+    const fetchData = async () => {
+        const response = await axios.get(`https://images-api.nasa.gov/search?q=${query}&media_type=image`);
 
-    useEffect(() => {
-        if (debouncedSearchTerm) {
-            setIsSearching(true);
+        setData(response.data.collection.items)
+        console.log(response.data.collection.items)
 
-            const fetchData = async () => {
-                const response = await axios.get(`https://images-api.nasa.gov/search?q=${query}&media_type=image`);
+        setQuery('')
+    }
+    const handleClick = () => {
+        fetchData();
+    }
 
-                setData(response.data.collection.items)
-                console.log(response.data.collection.items)
 
-                setIsSearching(false)
-                setQuery('')
-            }
-            fetchData();
-        }
-
-    }, [debouncedSearchTerm])
     return (
         <>
             <div className={styles.wrapper}>
@@ -39,9 +32,10 @@ const SearchView = () => {
                 <Form
                     query={query}
                     setQuery={setQuery}
+                    handleClick={handleClick}
                 />
+
             </div>
-            {isSearching && <p className={styles.searchInfo}>loading <span>. . . .</span></p>}
 
             <ImagesList data={data} />
         </>
